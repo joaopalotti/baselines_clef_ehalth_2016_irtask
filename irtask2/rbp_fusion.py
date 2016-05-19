ede620f936cc6c8ade2a4e4ca11828fbfa9f76f7
joaopalotti <joaopalotti@gmail.com>
2016-05-19 15:22:31 +0200

    Added support to parameters instead of hardcoded variables

--- irtask2/rbp_fusion.py
+++ irtask2/rbp_fusion.py
@@ -1,16 +1,24 @@
 from trectools import TrecRun
 
-outfilename="baseline.task2.BM25_EN_run1.dat"
+import sys
+
+if len(sys.argv) < 3:
+    print "Usage: python rbp_fusion.py <input_run> <outputname> <rbp_p>"
+    print "<input_run> can be a compressed file (bz2 or gzip) if you want to."
+    sys.exit(0)
+
+inputfile = sys.argv[1]
+outfilename = sys.argv[2]
+rbp_p = float(sys.argv[3])
 
 # Reads a run and save it in a pandas dataframe
-run = TrecRun("./baseline.BM25_EN_Run1.dat")
+run = TrecRun(inputfile)
 df = run.run_data
 
 # Get the qid for each group
 df["qid"]  = df["query"].apply(lambda x: str(x)[0:3])
 
 # Applies the rbp function to score documents for each qid group
-rbp_p = .80
 df["rbp_value"] = (1-rbp_p) * (rbp_p) ** (df["rank"]-1)
 grouped_by_docid = df.groupby(["qid","docid"])["rbp_value"].sum().reset_index()
 
